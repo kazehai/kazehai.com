7af530f7f39fec8dfe46213cbade9b3be27c41af
kazehai <4957248+kazehai@users.noreply.github.com>
2025-09-17 08:59:11 -0400

    Update eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,9 @@
 module.exports = (config) => {
+	// Set directories to pass through to the dist folder
+	config.addPassthroughCopy('./src/css/');
+	config.addPassthroughCopy('./src/images/');
+	config.addPassthroughCopy('./src/fonts/');
 	return {
-        markdownTemplateEngine: 'njk',
-        dataTemplateEngine: 'njk',
-        htmlTemplateEngine: 'njk',
 		dir: {
 			input: 'src',
 			output: 'dist',
